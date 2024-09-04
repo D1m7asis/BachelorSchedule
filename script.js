3eded70b23dfe917f2549b201c80a44336cbdc28
D1m7asis <tumaskow@yandex.ru>
2024-09-04 15:34:20 +0200

    Redeploy

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ $(document).ready(function () {
     function loadSchedule(modalId, key) {
         $.getJSON('schedule.json', function(data) {
             if (data[key]) {
-                var scheduleHtml = generateScheduleTable(data[key]);
+                const scheduleHtml = generateScheduleTable(data[key]);
                 $(modalId + ' .modal-body').html(scheduleHtml);
             } else {
                 $(modalId + ' .modal-body').html('Расписание не доступно');
